f909fc40fb7e6685c8d9adf508da3a3ad88b3701
Jules Goy <81039601+Witchoy@users.noreply.github.com>
2026-04-11 14:23:04 +0200

    feat(movement) Added basic player movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private readonly float _leftLimit = -6.0f;
+    private InputAction _moveAction;
+    private Vector2 _moveInput;
+
+    private Rigidbody _playerRigidbody;
+
+    private readonly float _rightLimit = 6.0f;
+
+    private void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+        if (_moveAction == null) Debug.LogError("Error occured while initializing move action");
+
+        _playerRigidbody = GetComponent<Rigidbody>();
+        if (_playerRigidbody == null) Debug.LogError("Error occurred while initializing rigidbody");
+    }
+
+    private void Update()
+    {
+        _moveInput = _moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _playerRigidbody.linearVelocity = new Vector3(
+            _moveInput.x * moveSpeed,
+            _playerRigidbody.linearVelocity.y,
+            moveSpeed
+        );
+
+        var pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, _leftLimit, _rightLimit);
+        transform.position = pos;
+    }
+}
